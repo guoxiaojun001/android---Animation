a5b7d40590711e2f58511edb74b6bbdf20833807
shuyun123 <1197173684@qq.com>
2015-07-12 16:34:05 +0800

    first add

---
+++ Carousel/src/com/example/carousel/ImageCycleView.java
@@ -0,0 +1,17 @@
+package com.example.carousel;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class ImageCycleView extends LinearLayout {
+
+	public ImageCycleView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+    
+	public ImageCycleView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+}
